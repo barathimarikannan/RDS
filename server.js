e7ee7cc9ac195d80c3f35b1c72de3686b53406d4
barathimarikannan <152593066+barathimarikannan@users.noreply.github.com>
2025-01-05 12:16:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-    host: 'database-1.c54c2w448eoa.ap-south-1.rds.amazonaws.com',
+    host: 'your-rds-endpoint.amazonaws.com',
     user: 'admin',
-    password: 'dhinabaru_39',
-    database: 'name'
+    password: 'yourpassword',
+    database: 'userdb'
 });
 
 db.connect(err => {
